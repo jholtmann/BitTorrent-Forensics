7485ed7d3340c63ecc4738cc4f96ad77eea1ffe0
Jonathan Holtmann <jonathan@holtmann.xyz>
2020-02-25 16:38:39 -0800

    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BitTorrent-Forensics
+Python script for analyzing .torrent files and uTorrent .dat files
+
+## Functionality
+
+1. Perform piece analysis on `.torrent` files to verify that a file/folder was downloaded using the `.torrent` file
+2. Retrieve a list of DHT peers from the `dht.dat` file
+3. Retrieve a list of peers from the `resume.dat` file
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bittorrent-forensics',
+    version='0.0.1',
+    description='Python project for analyzing BitTorrent forensic artifacts',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jholtmann/BitTorrent-Forensics',
+    author='Jonathan Holtmann',
+    author_email='jholtmann.contact@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='bittorrent utorrent forensics',
+    # package_dir={'': 'src'},
+    packages=find_packages(where='.'),
+    python_requires='>=3.3',
+    install_requires=[
+        'bencode-parser',
+        'colorama',
+        'tqdm',
+        'tabulate',
+        'termcolor'
+    ],
+    entry_points={
+        'console_scripts': [
+            'bittorrent-forensics=btf.btf:main'
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/jholtmann/BitTorrent-Forensics/issues',
+        'Source': 'https://github.com/jholtmann/BitTorrent-Forensics/tree/master/btf',
+    },
+)
